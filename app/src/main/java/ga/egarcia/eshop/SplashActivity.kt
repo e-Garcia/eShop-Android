7495adb8edaffb27c903e614ee3bbc070d84da3a
Erick Garcia <soporte.egarcia@gmail.com>
2019-01-26 14:18:08 -0700

    Added product flavors support for a clean separation between the open source application code and the proprietary visual assets of the client.

--- app/src/main/java/ga/egarcia/eshop/SplashActivity.kt
+++ app/src/main/java/ga/egarcia/eshop/SplashActivity.kt
@@ -1,5 +1,6 @@
 package ga.egarcia.eshop
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,5 +9,12 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent)
+
     }
 }
